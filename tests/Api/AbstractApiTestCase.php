b1f1155b3e13577fd56dfc5e647f0773d0bf89ae
Oliver Kaufmann <okaufmann91@gmail.com>
2016-11-14 21:42:38 +0100

    Extend API testsuite to test soft deleted entries

--- tests/Api/AbstractApiTestCase.php
+++ tests/Api/AbstractApiTestCase.php
@@ -25,4 +25,66 @@ abstract class AbstractApiTestCase extends AbstractTestCase
 
         return $this->apiUri.'?'.$paramString;
     }
+
+    protected function seeSuccess()
+    {
+        return $this->seeStatusCode(200)
+            ->see($this->success);
+    }
+
+    /**
+     * Assert that a given where condition does not matches a soft deleted record
+     * From: http://stackoverflow.com/questions/33117746/laravel-unit-testing-how-to-seeindatabase-soft-deleted-row
+     *
+     * @param  string $table
+     * @param  array $data
+     * @param  string $connection
+     * @return $this
+     */
+    protected function seeIsNotSoftDeletedInDatabase($table, array $data, $connection = null)
+    {
+        $database = $this->app->make('db');
+
+        $connection = $connection ?: $database->getDefaultConnection();
+
+        $count = $database->connection($connection)
+            ->table($table)
+            ->where($data)
+            ->whereNull('deleted_at')
+            ->count();
+
+        $this->assertGreaterThan(0, $count, sprintf(
+            'Found unexpected records in database table [%s] that matched attributes [%s].', $table, json_encode($data)
+        ));
+
+        return $this;
+    }
+
+    /**
+     * Assert that a given where condition matches a soft deleted record
+     * From: http://stackoverflow.com/questions/33117746/laravel-unit-testing-how-to-seeindatabase-soft-deleted-row
+     *
+     * @param  string $table
+     * @param  array $data
+     * @param  string $connection
+     * @return $this
+     */
+    protected function seeIsSoftDeletedInDatabase($table, array $data, $connection = null)
+    {
+        $database = $this->app->make('db');
+
+        $connection = $connection ?: $database->getDefaultConnection();
+
+        $count = $database->connection($connection)
+            ->table($table)
+            ->where($data)
+            ->whereNotNull('deleted_at')
+            ->count();
+
+        $this->assertGreaterThan(0, $count, sprintf(
+            'Found unexpected records in database table [%s] that matched attributes [%s].', $table, json_encode($data)
+        ));
+
+        return $this;
+    }
 }
