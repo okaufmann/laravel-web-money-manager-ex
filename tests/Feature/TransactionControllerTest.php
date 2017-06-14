46794df2d9b8bdb656179ea2ddbc7b9b05b9dbab
Oliver Kaufmann <okaufmann91@gmail.com>
2017-06-14 21:26:05 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Feature/TransactionControllerTest.php
+++ tests/Feature/TransactionControllerTest.php
@@ -3,15 +3,14 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class TransactionControllerTest extends TestCase
 {
     /**
      * A basic test example.
+     *
      * @test
+     *
      * @return void
      */
     public function it_can_browse_transaction_create_form()
@@ -23,7 +22,6 @@ class TransactionControllerTest extends TestCase
 
         // Assert
         $response->assertStatus(200)
-            ->assertSee("Add new Transaction");
-
+            ->assertSee('Add new Transaction');
     }
 }
