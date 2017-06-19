9e3ff4b3ea9822591ae82b7361bf7adedde9baae
Oliver Kaufmann <okaufmann91@gmail.com>
2017-06-20 01:38:36 +0200

    Go back to minimal test setuo

--- tests/Feature/TestCase.php
+++ tests/Feature/TestCase.php
@@ -2,25 +2,27 @@
 
 namespace Tests\Features;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\UsesDatabase;
 use Tests\utils\DbUtils;
 use UsersTableSeeder;
 
 abstract class TestCase extends \Tests\TestCase
 {
-    use UsesDatabase;
+    //use UsesDatabase;
+    use DatabaseMigrations;
     use DbUtils;
 
     public function setUp()
     {
-        $this->prepareDatabase(true);
+        //$this->prepareDatabase(true);
 
         parent::setUp();
 
-        $this->setUpDatabase(function () {
-            $this->artisan('db:seed', ['--class' => UsersTableSeeder::class]);
-        });
+        //$this->setUpDatabase(function () {
+        //    $this->artisan('db:seed', ['--class' => UsersTableSeeder::class]);
+        //});
 
-        $this->beginDatabaseTransaction();
+        //$this->beginDatabaseTransaction();
     }
 }
