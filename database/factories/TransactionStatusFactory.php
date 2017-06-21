a4e4716b1ba99a4655f49a1a81b415e59f63fcbd
Oliver Kaufmann <okaufmann91@gmail.com>
2017-06-22 01:08:08 +0200

    Align name and slug

--- database/factories/TransactionStatusFactory.php
+++ database/factories/TransactionStatusFactory.php
@@ -14,8 +14,9 @@ $factory->define(App\Models\TransactionStatus::class, function (Faker\Generator 
      * $StatusArrayDesc = array ("None", "Reconciled", "Void", "Follow Up", "Duplicate");
      * $StatusArrayDB = array ("", "R", "V", "F", "D");
      */
+    $name = implode('_', $faker->words(2));
     return [
-        'name' => $faker->word,
-        'slug' => str_slug(implode('_', $faker->words(2))),
+        'name' => $name,
+        'slug' => str_slug($name),
     ];
 });
--- database/factories/TransactionTypeFactory.php
+++ database/factories/TransactionTypeFactory.php
@@ -10,8 +10,9 @@
  */
 $factory->define(App\Models\TransactionType::class, function (Faker\Generator $faker) {
     // $TypeArrayDesc = array ("Withdrawal", "Deposit", "Transfer");
+    $name = implode('_', $faker->words(2));
     return [
-        'name' => $faker->word,
-        'slug' => str_slug(implode('_', $faker->words(2))),
+        'name' => $name,
+        'slug' => str_slug($name),
     ];
 });
