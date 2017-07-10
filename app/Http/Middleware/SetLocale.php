13543e3c3f35d018f14d70ec461521d9be96cfe3
Oliver Kaufmann <okaufmann91@gmail.com>
2017-07-11 00:28:20 +0200

    Set app locale by user setting

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App;
+use Closure;
+
+class SetLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($user = $request->user()) {
+            if ($user->language) {
+                App::setLocale($user->language);
+            }
+        }
+        return $next($request);
+    }
+}
