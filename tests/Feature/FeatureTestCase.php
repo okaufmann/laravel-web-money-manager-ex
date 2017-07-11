2722f4612c04d52856f4513d31e54781120eb2a2
Oliver Kaufmann <okaufmann91@gmail.com>
2017-07-11 16:18:14 +0200

    Rename Feature/TestCase to Feature/FeatureTestCase

---
+++ tests/Feature/FeatureTestCase.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Features;
+
+use App\Models\User;
+use Tests\UsesDatabase;
+use Tests\utils\DbUtils;
+use UsersTableSeeder;
+
+abstract class FeatureTestCase extends \Tests\TestCase
+{
+    use UsesDatabase;
+    use DbUtils;
+
+    /** @var User */
+    public $user;
+
+
+    public function setUp()
+    {
+        $this->prepareDatabase();
+
+        parent::setUp();
+
+        $this->setUpDatabase(function () {
+            $this->artisan('db:seed', ['--class' => UsersTableSeeder::class]);
+        });
+
+        $this->beginDatabaseTransaction();
+    }
+
+    /**
+     * Create and return a new user.
+     *
+     * @param array $properties
+     *
+     * @return \App\Models\User
+     */
+    protected function ensureUser($properties = [])
+    {
+        $this->user = factory(User::class)->create($properties);
+    }
+
+    protected function ensureAuthenticated()
+    {
+        $this->ensureUser();
+        $this->actingAs($this->user);
+        $this->actingAs($this->user, 'api');
+    }
+}
