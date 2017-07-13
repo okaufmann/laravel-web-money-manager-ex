c0bc361ee66855b274682fa01efb89c404143c87
Oliver Kaufmann <okaufmann@users.noreply.github.com>
2017-07-13 10:38:54 +0200

    Make column user_id unsigned

--- database/migrations/2016_10_22_131255_create_accounts_table.php
+++ database/migrations/2016_10_22_131255_create_accounts_table.php
@@ -15,7 +15,7 @@ class CreateAccountsTable extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned();;
             $table->string('name');
             $table->timestamps();
         });
