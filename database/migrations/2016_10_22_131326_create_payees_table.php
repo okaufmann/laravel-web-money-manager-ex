4b687b0a424f5918bd5aeb5babbc9ae1fcea1d4e
Oliver Kaufmann <okaufmann@users.noreply.github.com>
2017-07-13 10:39:19 +0200

    Make column user_id unsigned

--- database/migrations/2016_10_22_131326_create_payees_table.php
+++ database/migrations/2016_10_22_131326_create_payees_table.php
@@ -15,7 +15,7 @@ class CreatePayeesTable extends Migration
     {
         Schema::create('payees', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned();
             $table->integer('last_category_id')->unsigned()->nullable();
             $table->string('name');
             $table->timestamps();
