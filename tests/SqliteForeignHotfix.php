19bcd837c1cce782df7ef0ec1546570bc720fac9
Oliver Kaufmann <okaufmann91@gmail.com>
2018-11-23 20:44:26 +0100

    add hotfix for sqlite foreign key problem
    https://github.com/laravel/framework/issues/25475

--- tests/SqliteForeignHotfix.php
+++ tests/SqliteForeignHotfix.php
@@ -4,7 +4,39 @@
 namespace Tests;
 
 
+use Closure;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\SQLiteBuilder;
+use Illuminate\Database\SQLiteConnection;
+use Illuminate\Support\Fluent;
+
 trait SqliteForeignHotfix
 {
-
+    /**
+     *
+     */
+    public function hotfixSqlite()
+    {
+        \Illuminate\Database\Connection::resolverFor('sqlite', function ($connection, $database, $prefix, $config) {
+            return new class($connection, $database, $prefix, $config) extends SQLiteConnection {
+                public function getSchemaBuilder()
+                {
+                    if ($this->schemaGrammar === null) {
+                        $this->useDefaultSchemaGrammar();
+                    }
+                    return new class($this) extends SQLiteBuilder {
+                        protected function createBlueprint($table, Closure $callback = null)
+                        {
+                            return new class($table, $callback) extends Blueprint {
+                                public function dropForeign($index)
+                                {
+                                    return new Fluent();
+                                }
+                            };
+                        }
+                    };
+                }
+            };
+        });
+    }
 }
