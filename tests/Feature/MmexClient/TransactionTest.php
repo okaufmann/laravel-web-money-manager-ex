4cc3fbdfa6a6ee5765e4349c5a894e2c057ed3ea
Oliver Kaufmann <okaufmann91@gmail.com>
2018-11-23 21:59:25 +0100

    use more impressive method names

--- tests/Feature/MmexClient/TransactionTest.php
+++ tests/Feature/MmexClient/TransactionTest.php
@@ -16,7 +16,8 @@ use App\Services\TransactionService;
 
 class TransactionTest extends MmexTestCase
 {
-    public function testEmptyResponseWhenNoTransactionsExists()
+    /** @test */
+    public function it_returns_an_empty_response_if_no_transactions_does_exist()
     {
         // Arrange
         $url = $this->buildUrl(['download_transaction' => 'true']);
@@ -29,7 +30,8 @@ class TransactionTest extends MmexTestCase
         $this->assertEquals($response->getContent(), '');
     }
 
-    public function testDeleteTransactions()
+    /** @test */
+    public function it_can_delete_transactions()
     {
         // Arrange
         $transaction = factory(Transaction::class)->create(['user_id' => $this->user->id]);
@@ -43,7 +45,8 @@ class TransactionTest extends MmexTestCase
         $this->assertIsSoftDeletedInDatabase('transactions', ['user_id' => $this->user->id, 'id' => $transaction->id]); // must not be deleted! just soft deleted.
     }
 
-    public function testDownloadTransactions()
+    /** @test */
+    public function it_can_download_transactions()
     {
         // Arrange
         /** @var Transaction $transaction */
@@ -58,32 +61,33 @@ class TransactionTest extends MmexTestCase
         $response->assertStatus(200)
             ->assertJsonFragment(
                 [
-                    'ID'          => (string) $transaction->id,
-                    'Date'        => $transaction->transaction_date->toDateString(),
-                    'Account'     => $transaction->account_name,
-                    'ToAccount'   => $transaction->to_account_name,
-                    'Status'      => $transaction->status->slug,
-                    'Type'        => $transaction->type->name,
-                    'Payee'       => $transaction->payee_name,
-                    'Category'    => $transaction->category_name,
+                    'ID' => (string)$transaction->id,
+                    'Date' => $transaction->transaction_date->toDateString(),
+                    'Account' => $transaction->account_name,
+                    'ToAccount' => $transaction->to_account_name,
+                    'Status' => $transaction->status->slug,
+                    'Type' => $transaction->type->name,
+                    'Payee' => $transaction->payee_name,
+                    'Category' => $transaction->category_name,
                     'SubCategory' => $transaction->sub_category_name,
-                    'Amount'      => (string) $transaction->amount,
-                    'Notes'       => $transaction->notes,
+                    'Amount' => (string)$transaction->amount,
+                    'Notes' => $transaction->notes,
                     'Attachments' => 'Transaction_'.$transaction->id.'_test-receipt.png;Transaction_'.$transaction->id
                         .'_test-receipt-2.png;Transaction_'.$transaction->id.'_test-receipt-3.png',
                 ]
             );
     }
 
-    /**
-     * Attachment file name will be provided as comma separated list in the transaction download.
-     */
-    public function testDownloadAttachment()
+
+    /** @test */
+    public function it_can_download_attachments()
     {
         // Arrange
         /** @var Transaction $transaction */
         $transaction = factory(Transaction::class)->create(['user_id' => $this->user->id]);
         $this->addReceiptsToTransaction($transaction);
+        
+        // Attachment file name will be provided as comma separated list in the transaction download.
         $fileName = 'Transaction_'.$transaction->id.'_test-receipt-3.png';
         $url = $this->buildUrl(['download_attachment' => $fileName]);
 
