22eebeb8748fd5fffbe1902c68304a554e8aab74
Oliver Kaufmann <okaufmann91@gmail.com>
2018-11-23 23:13:46 +0100

    ?

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -9,12 +9,16 @@
  * @version 1.0
  */
 $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
+    $guid = mmex_guid();
+    \Log::debug(sprintf('creating new user with uuid: `%s`', $guid));
+
+
     return [
         'name'      => $faker->name,
         'email'     => $faker->email,
         'password'  => bcrypt('secret'),
         'is_admin'  => true,
-        'mmex_guid' => mmex_guid(),
+        'mmex_guid' => $guid,
         'locale'    => 'en_US',
         'api_token' => str_random(60),
     ];
