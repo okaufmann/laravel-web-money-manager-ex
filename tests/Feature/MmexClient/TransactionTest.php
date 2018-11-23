b1d9ebdc1cbf514cdd6bffd409d4f2f0bb4db5fa
Oliver Kaufmann <okaufmann91@gmail.com>
2018-11-23 21:02:23 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Feature/MmexClient/TransactionTest.php
+++ tests/Feature/MmexClient/TransactionTest.php
@@ -61,7 +61,7 @@ class TransactionTest extends MmexTestCase
         $response->assertStatus(200)
             ->assertJsonFragment(
                 [
-                    'ID' => (string)$transaction->id,
+                    'ID' => (string) $transaction->id,
                     'Date' => $transaction->transaction_date->toDateString(),
                     'Account' => $transaction->account_name,
                     'ToAccount' => $transaction->to_account_name,
@@ -70,14 +70,13 @@ class TransactionTest extends MmexTestCase
                     'Payee' => $transaction->payee_name,
                     'Category' => $transaction->category_name,
                     'SubCategory' => $transaction->sub_category_name,
-                    'Amount' => (string)$transaction->amount,
+                    'Amount' => (string) $transaction->amount,
                     'Notes' => $transaction->notes,
                     'Attachments' => 'Transaction_'.$transaction->id.'_test-receipt.png;Transaction_'.$transaction->id
                         .'_test-receipt-2.png;Transaction_'.$transaction->id.'_test-receipt-3.png',
                 ]
             );
     }
-
 
     /** @test */
     public function it_can_download_attachments()
@@ -86,7 +85,7 @@ class TransactionTest extends MmexTestCase
         /** @var Transaction $transaction */
         $transaction = factory(Transaction::class)->create(['user_id' => $this->user->id]);
         $this->addReceiptsToTransaction($transaction);
-        
+
         // Attachment file name will be provided as comma separated list in the transaction download.
         $fileName = 'Transaction_'.$transaction->id.'_test-receipt-3.png';
         $url = $this->buildUrl(['download_attachment' => $fileName]);
